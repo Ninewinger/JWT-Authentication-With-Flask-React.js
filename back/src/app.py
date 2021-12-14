d28c64e48915f14dbf99a1184a7cdc27ffcb4b45
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2021-12-14 17:16:01 -0300

    yuju

--- back/src/app.py
+++ back/src/app.py
@@ -4,3 +4,22 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from models import db
 
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config["env"] = "development"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///back/db/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super-secret'
+db.init_app(app)
+migrate = Migrate(app, db)
+CORS(app)
+app.secret_key = 'super secret key'
+jwt = JWTManager(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
