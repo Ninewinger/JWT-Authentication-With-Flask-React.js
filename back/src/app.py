ffe9c28bfbc13ae75c46981c7aff1bf2afcdb373
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2021-12-15 17:39:36 -0300

    avances

--- back/src/app.py
+++ back/src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
-from models import db
+from models import db, User
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -21,5 +21,39 @@ jwt = JWTManager(app)
 def index():
     return render_template('index.html')
 
+@app.route('/signup', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data['username']
+    email = data['email']
+    password = data['password']
+
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return jsonify({'message': 'Username already exists'}), 400
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return jsonify({'message': 'Email already exists'}), 400
+
+    user = User(username=username, email=email, password=password)
+    user.save()
+    return jsonify({'message': 'User created successfully'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return jsonify({'message': 'User or password does not exist'}), 400
+
+    if user.password != password:
+        return jsonify({'message': 'User or password does not exist'}), 400
+
+    return jsonify({'token': create_access_token(identity=user.id)}), 200
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- back/src/models.py
+++ back/src/models.py
@@ -1,3 +1,33 @@
-from flask_sqlalchemy import SQLAlchemy;
+from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy();
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+    def update(self):
+        db.session.commit()
+
+    
